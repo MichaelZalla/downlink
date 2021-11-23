0597ce69dfcf824eb85214d2622d585a3416d654
Michael Zalla <michael@zalla.io>
2021-11-23 00:17:14 -0800

    Uses string literal;

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -14,41 +14,48 @@ console.clear()
 // console.log(jsonToTs(parcels))
 // console.log(jsonToTs(prices))
 
-const myData: string = JSON.stringify(
-	{
-		foo: true,
-		bar: 3.14,
-		hype: 'beast',
-		favorites: ['breakfast','lunch','dinner'],
-		clients: [
-			{
-				id: 1,
-				name: 'Stan',
-				'contact-info': {
-					phone: '+1 (111) 111-1111',
-					email: 'stan@company.com',
-				},
-			},
-			{
-				id: 2,
-				name: 'Beth',
-				'contact-info': {
-					phone: '+1 (222) 222-2222',
-					email: 'bethany@company.com',
-				},
-			},
-			{
-				id: 3,
-				name: null,
-				'contact-info': {
-					phone: '+1 (333) 333-3333',
-					email: 'admin@company.com',
-				},
-				isAdmin: true,
-			}
-		],
-		yikes: [true, 2, 'three']
-	}
-)
-
-console.log(jsonToTs(myData))
+const someJson: string = `
+{
+    "foo": true,
+    "bar": 3.14,
+    "hype": "beast",
+    "favorites": [
+        "breakfast",
+        "lunch",
+        "dinner"
+    ],
+    "clients": [
+        {
+            "id": 1,
+            "name": "Stan",
+            "contact-info": {
+                "phone": "+1 (111) 111-1111",
+                "email": "stan@company.com"
+            }
+        },
+        {
+            "id": 2,
+            "name": "Beth",
+            "contact-info": {
+                "phone": "+1 (222) 222-2222",
+                "email": "bethany@company.com"
+            }
+        },
+        {
+            "id": 3,
+            "name": null,
+            "contact-info": {
+                "phone": "+1 (333) 333-3333",
+                "email": "admin@company.com"
+            },
+            "isAdmin": true
+        }
+    ],
+    "yikes": [
+        true,
+        2,
+        "three"
+    ]
+}
+`
+console.log(jsonToTs(someJson))
